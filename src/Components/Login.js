86c680d221ba1e930a2400719e6865bce101d705
afrolambo <stephennoahgalvan@gmail.com>
2021-04-19 18:59:02 -0400

    install tailwind css with create react app guide

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div>
-            
-        </div>
+        <figure className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
+            <div className="pt-6 md:p-8 text-center md:text-left space-y-4"> 
+            <h3> Login </h3>
+            <blockquote>
+                <p className="text-lg font-semibold" >Welcome to Diversify</p>
+            </blockquote>
+            </div> 
+        </figure>
     )
 }
