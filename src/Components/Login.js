abca38ee304457f4bd48f544dc4bbb44482cb889
afrolambo <stephennoahgalvan@gmail.com>
2021-04-20 18:38:59 -0400

    Styling for Login form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,14 +7,13 @@ export default function Login({ login, register }) {
     const [isRegistered, setIsRegistered] = useState(true)
 
     return (
-        <figure className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
-            <div className="pt-6 md:p-8 text-center md:text-left space-y-4"> 
-            <h3> Login </h3>
-            <blockquote>
-                <p className="text-lg font-semibold" >Welcome to Diversify</p>
-            </blockquote>
+        <figure className="h-screen flex bg-gray-100">
+            <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-1"> 
+                <blockquote className="text-2xl font-medium text-center">
+                        <p className="text-lg font-semibold" >Welcome to Diversify</p>
+                </blockquote>
+                {isRegistered ? <LoginForm login={login} setIsRegistered={setIsRegistered} /> : <RegistrationForm register={register} setIsRegistered={setIsRegistered} />}
             </div> 
-            {isRegistered ? <LoginForm login={login} setIsRegistered={setIsRegistered} /> : <RegistrationForm register={register} setIsRegistered={setIsRegistered} />}
         </figure>
     )
 }
--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -21,15 +21,17 @@ export default function LoginForm({ login, setIsRegistered }) {
     }
 
     return (
-        <div>
-            <h1>LOGIN FORM</h1>
+        <div className="text-primary m-6">
+            <h1 className="text-1xl font-medium text-primary mt-4 mb-12 text-center">Login to your account</h1>
             <form onSubmit={handleSubmit}>
-                <label>Username:</label>
+                <label className="text-left">Username:</label>
                 <input
                     name="name"
                     type="text"
                     value={formData.name}
                     onChange={handleChange}
+                    placeholder="username"
+                    className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"}
                 />
                 <label>Password:</label>
                 <input
@@ -37,10 +39,18 @@ export default function LoginForm({ login, setIsRegistered }) {
                     type="password"
                     value={formData.password}
                     onChange={handleChange}
+                    placeholder="password"
+                    className={"w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4"}
                 />
-                <input type="submit" value="Login" />
+                <div className="flex items-center mt-3">   
+                    <button 
+                        className={"bg-blue-700 hover:bg-blue-500 py-2 px-4 text-md text-white rounded border border-blue focus:outline-none focus:border-black"} 
+                        value="Login">
+                            Login
+                    </button>
+                </div>
             </form> 
-            <button onClick={handleClick}>Need to register?</button>
+            <button className={"text-blue-500 hover:underline"} onClick={handleClick}>Need to register? Sign up for free</button>
         </div>
     )
 }
