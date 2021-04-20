3d3ccdec4e145a00a3a7bf2d08a9ae90f9834272
gjeffgolden <gjgolden08@gmail.com>
2021-04-20 11:35:35 -0600

    Connected FE auth, changed FE port to 3001.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 // import './App.css';
+import React, { useState, useEffect } from 'react'
 import Login from './Components/Login'
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const baseUrl = "http://localhost:3000"
+
+  const login = (user) => {
+    fetch(`${baseUrl}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    setIsLoggedIn(true)
+  }
+
+  useEffect(() => {
+    if (localStorage.token) {
+      fetch(`${baseUrl}/profile`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${localStorage.token}`
+        }
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      setIsLoggedIn(true)
+    }
+  }, [])
+
   return (
     <div className="App">
       <div className="text-red-900">Hello</div>
-      <Login />
+      {isLoggedIn ? <h1>You're logged in.</h1> : <Login login={login} />}
     </div>
   );
 }
