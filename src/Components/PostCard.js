65dfb187de2592ec52a4b18b7d44d20a1764d0e5
gjeffgolden <gjgolden08@gmail.com>
2021-04-22 12:44:35 -0600

    Created Posts Container and Post Cards.

---
+++ src/Components/PostCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PostCard() {
+    return (
+        <container className="flex flex-col items-center justify-center w-full h-1/6 border-solid border-4 border-blue-200">
+            {/* ADD USERNAME */}
+            <h1>Post Title</h1>
+            <h2>Post Content</h2>
+        </container>
+    )
+}
