6dbfe7bd712996736aa1491392d4c1c00f6919b4
gjeffgolden <gjgolden08@gmail.com>
2021-04-22 11:28:00 -0600

    Added Chatbox, form needs completion.

---
+++ src/Components/ChatBox.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+export default function ChatBox() {
+
+    const [formData, setFormData] = useState({
+        title: "",
+        content: "",
+        ip_address: "",
+        user_id: "",
+        community_id: "",
+    })
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    return (
+        <section className="flex flex-row h-1/5 md:w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0 border-solid border-4 border-green-200">
+            <form className="flex flex-col w-full h-full items-center justify-evenly">
+                <input className="border border-black w-3/4 h-8 p-0.5" type="text" name="title" placeholder="Post Title" onChange={handleChange} />
+                <input className="border border-black w-3/4 h-8 p-0.5" type="text" name="content" placeholder="Message" onChange={handleChange} />
+                <input className="w-48 h-8" type="submit" value="Post" />
+            </form>
+        </section>
+    )
+}
--- src/Containers/MainPage.js
+++ src/Containers/MainPage.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import SideBar from './SideBar';
-import NavBar from '../Components/NavBar'
+import NavBar from '../Components/NavBar';
+import ChatBox from '../Components/ChatBox';
 
 export default function MainPage({communities}) {
     return (
             <div className="md:flex flex-col md:flex-row md:min-h-screen md:min-w-screen pl-5 pt-3">
                 <SideBar communities={communities}/>
-                <container className="flex flex-col w-full md:w-9/12 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0 border-solid border-4 border-red-200 ml-2" >
+                <container className="flex flex-col justify-between w-full md:w-9/12 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0 border-solid border-4 border-red-200 ml-2" >
                     <NavBar />
-                    You're logged in.
+                    <ChatBox />
                 </container>
             </div>
     )
