af1ffcafe4f96aa77736e7a902fe22dd39ab59c9
Jack Hubert <jphubert93@gmail.com>
2021-04-23 17:47:28 -0600

    still need a function to identify if a user is a member of hte community or not in order to display 'leave' or 'join'

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -20,11 +20,45 @@ export default function Navbar({ community , currentUser, navbarOpen}) {
       console.log(response);
     })
   }
-   
+
+  const leave = () => {
+    const communityId = community.id
+    const userId = currentUser.id
+
+    fetch(`${baseUrl}/user_communities`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userId, communityId)
+    })
+    .then((response) => {
+      response.json();
+      console.log(response);
+    })
+  }
+
+  const isCommunityMember = () => {
+    // Create a function to check if 
+    // currentUser is member of Community
+    // if user is a member of the community, return true
+    // else return false
+  }
+
+  const handleCommunityMembership = () => {
+    return (
+      isCommunityMember
+      ? leaveButton()
+      : joinButton()
+    )
+  }
+
   const joinButton = () => {
     return (
       <li className="nav-item">
         <a
+          onClick={join}
           className="
             px-3
             py-2
@@ -53,6 +87,7 @@ export default function Navbar({ community , currentUser, navbarOpen}) {
     return (
       <li className="nav-item">
         <a
+          onClick={leave}
           className="
             px-3
             py-2
@@ -77,9 +112,108 @@ export default function Navbar({ community , currentUser, navbarOpen}) {
     )
   }
 
-  const handleCommunityMembership = () => {
+  const communityNavBar = () => {
     return (
-      leaveButton()
+      <>
+        <ul
+          className="
+            flex
+            flex-col
+            lg:flex-row
+            list-none
+            lg:ml-auto
+          ">
+          <li className="nav-item">
+            <a
+              className="
+                px-3
+                py-2
+                flex
+                items-center
+                text-xs
+                font-bold
+                leading-snug
+                hover:opacity-75
+                text-white
+              "
+              href="#"
+            >
+              <span className="ml-2">
+                View All Members(Modal)
+              </span>
+            </a>
+          </li>
+        </ul>
+        <ul
+          className="
+            flex
+            flex-col
+            lg:flex-row
+            list-none
+            lg:ml-auto
+          ">
+          { handleCommunityMembership() }
+          <li className="sign-up">
+            <a
+              className="
+                px-3
+                py-2
+                flex
+                items-center
+                text-xs
+                font-bold
+                leading-snug
+                hover:opacity-75
+                text-white
+              "
+              href="#"
+            >
+            <div className="inline-flex mt-2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+              </svg>
+              <span className="pl-1"> details </span> 
+            </div> 
+            </a>
+          </li>
+        </ul>
+      </>
+    )
+  }
+
+  const communitySelect = () => {
+    return (
+      <>
+                <ul
+          className="
+            flex
+            flex-col
+            lg:flex-row
+            list-none
+            lg:ml-auto
+          ">
+          <li className="nav-item">
+            <a
+              className="
+                px-3
+                py-2
+                flex
+                items-center
+                text-xs
+                font-bold
+                leading-snug
+                hover:opacity-75
+                text-white
+              "
+              href="#"
+            >
+              <span className="ml-2">
+                Please select a community, or create one of your own!
+              </span>
+            </a>
+          </li>
+        </ul>
+      </>
     )
   }
 
@@ -118,73 +252,17 @@ export default function Navbar({ community , currentUser, navbarOpen}) {
           </div>
           <div
             className={
-              "lg:flex flex-grow items-center" +
-              (navbarOpen ? " flex" : " hidden")
+              "lg:flex flex-grow items-center flex"
+              // +
+              // (navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
-          >
-            <ul
-              className="
-                flex
-                flex-col
-                lg:flex-row
-                list-none
-                lg:ml-auto
-              ">
-              <li className="nav-item">
-                <a
-                  className="
-                    px-3
-                    py-2
-                    flex
-                    items-center
-                    text-xs
-                    font-bold
-                    leading-snug
-                    hover:opacity-75
-                    text-white
-                  "
-                  href="#"
-                >
-                  <span className="ml-2">
-                    View All Members(Modal)
-                  </span>
-                </a>
-              </li>
-            </ul>
-            <ul
-              className="
-                flex
-                flex-col
-                lg:flex-row
-                list-none
-                lg:ml-auto
-              ">
-              { handleCommunityMembership() }
-              <li className="sign-up">
-                <a
-                  className="
-                    px-3
-                    py-2
-                    flex
-                    items-center
-                    text-xs
-                    font-bold
-                    leading-snug
-                    hover:opacity-75
-                    text-white
-                  "
-                  href="#"
-                >
-                <div className="inline-flex mt-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-                  </svg>
-                  <span className="pl-1"> details </span> 
-                </div> 
-                </a>
-              </li>
-            </ul>
+          >    
+          {
+            navbarOpen
+            ? communityNavBar()
+            : communitySelect()
+          }
           </div>
         </div>
       </nav>
